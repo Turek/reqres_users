40f1035a9a3cd8aa713672e1a75daa36b235f10e
Tomasz Turczynski <tomasz@turczynski.com>
2024-03-27 01:22:15 +0100

    Add caching to the service.

---
+++ src/EventSubscriber/PostMigrationSubscriber.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\reqres_users\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Drupal\migrate\Event\MigrateEvents;
+use Drupal\migrate\Event\MigrateImportEvent;
+use Drupal\Core\Cache\Cache;
+
+/**
+ * Class PostMigrationSubscriber.
+ */
+class PostMigrationSubscriber implements EventSubscriberInterface {
+
+  /**
+   * Post migration event subscriber to invalidate caches when new content
+   * was added or updated.
+   *
+   * @param MigrateImportEvent $event
+   *   The migration event.
+   */
+  public function onPostImport(MigrateImportEvent $event) {
+    $migration = $event->getMigration();
+    $id_map = $migration->getIdMap();
+
+    // Check if there were newly imported or updated items.
+    $imported = $id_map->importedCount();
+    $updated = $id_map->updatedCount();
+
+    // Only invalidate cache if there were updates or new imports.
+    if ($imported > 0 || $updated > 0) {
+      // Specify the cache tags you want to invalidate
+      $tags = ['reqres_users_migrate'];
+
+      // Invalidate cache tags
+      Cache::invalidateTags($tags);
+
+      // Optionally, log this action.
+      \Drupal::logger('custom_migration')->info('Cache tags invalidated after migration.');
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    // Subscribe to the post-import event.
+    return [
+      MigrateEvents::POST_IMPORT => ['onPostImport'],
+    ];
+  }
+
+}
--- src/Service/ReqresUserService.php
+++ src/Service/ReqresUserService.php
@@ -3,6 +3,10 @@
 namespace Drupal\reqres_users\Service;
 
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\reqres_users\Event\ReqresUsersAlterEvent;
+use Drupal\Core\Cache\CacheBackendInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Exception;
 
 /**
  * Provides a service for database connectivity.
@@ -10,20 +14,40 @@ use Drupal\Core\Entity\EntityTypeManagerInterface;
 class ReqresUserService {
 
   /**
+   * The cache backend instance.
+   *
+   * @var CacheBackendInterface
+   */
+  protected $cache;
+
+  /**
    * The entity type manager service.
    *
-   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   * @var EntityTypeManagerInterface
    */
   protected $entityTypeManager;
 
   /**
-   * Constructs a new ReqresUserService service object.
+   * The event dispatcher service.
    *
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
-   *   The entity type manager service.
+   * @var EventDispatcherInterface
    */
-  public function __construct(EntityTypeManagerInterface $entityTypeManager) {
+  protected $eventDispatcher;
+
+  /**
+   * Constructs a ReqresApiService object.
+   *
+   * @param CacheBackendInterface $cache
+   *   The cache backend service.
+   * @param EntityTypeManagerInterface $entityTypeManager
+   *   The entity type manager service.
+   * @param EventDispatcherInterface $eventDispatcher
+   *   The event dispatcher service.
+   */
+  public function __construct(CacheBackendInterface $cache, EntityTypeManagerInterface $entityTypeManager, EventDispatcherInterface $eventDispatcher) {
+    $this->cache = $cache;
     $this->entityTypeManager = $entityTypeManager;
+    $this->eventDispatcher = $eventDispatcher;
   }
 
   /**
@@ -37,28 +61,79 @@ class ReqresUserService {
    *   Return array of user objects.
    */
   public function getUsers(int $limit, int $page) {
-    // Query users with pagination.
-    $query = $this->entityTypeManager->getStorage('reqres_user')->getQuery();
-    $query->pager($limit);
-    $offset = $page * $limit;
-    $query->range($offset, $limit);
-    // Add tag to the query for altering.
-    $query->addTag('reqres_users_query_alter');
-    // Execute the query.
-    $results = $query->execute();
+    $cid = 'reqres_users:page_' . $page . ':limit_' . $limit;
 
-    // Load user entities.
-    $users = $this->entityTypeManager->getStorage('reqres_user')->loadMultiple($results);
+    // Attempt to retrieve from cache first.
+    if ($cache = $this->cache->get($cid)) {
+      return $cache->data;
+    }
 
-    // Allow other modules to alter the user list through hook.
-    \Drupal::moduleHandler()->alter('reqres_users_data', $users);
+    try {
+      // Query users with pagination.
+      $query = $this->entityTypeManager->getStorage('reqres_user')->getQuery();
+      $offset = $page === 0 ? 0 : $page * $limit;
+      $query->range($offset, $limit);
+      // Add tag to the query for altering.
+      $query->addTag('reqres_users_query');
+      $query->accessCheck(TRUE);
+      // Execute the query.
+      $results = $query->execute();
 
-    // Dispatch an event to allow altering the user list.
-    $event = new ReqresUsersAlterEvent($users);
-    $this->eventDispatcher()->dispatch(ReqresUsersAlterEvent::EVENT_NAME, $event);
+      // Load user entities.
+      $users = $this->entityTypeManager->getStorage('reqres_user')->loadMultiple($results);
 
-    // Get the altered user list and return it.
-    return $event->getUsers();
+      // Allow other modules to alter the user list through hook.
+      \Drupal::moduleHandler()->alter('reqres_users_data', $users);
+
+      // Dispatch an event to allow altering the user list.
+      $event = new ReqresUsersAlterEvent($users);
+      $this->eventDispatcher->dispatch($event, ReqresUsersAlterEvent::USERS_ALTER);
+
+      // Cache successful API response.
+      $this->cache->set($cid, $event->getUsers(), CacheBackendInterface::CACHE_PERMANENT, ['reqres_users_data']);
+
+      // Get the altered user list and return it.
+      return $event->getUsers();
+    }
+    catch (Exception $e) {
+      \Drupal::logger('reqres_users')->error('Failed fetching users from Reqres API: @message', ['@message' => $e->getMessage()]);
+
+      return [];
+    }
+  }
+
+  /**
+   * Get the total number of rows in the reqres_user entity table.
+   *
+   * @return int
+   *   The total number of rows.
+   */
+  public function getTotalRows() {
+    $cid = 'reqres_users:total_rows';
+
+    // Attempt to retrieve from cache first.
+    if ($cache = $this->cache->get($cid)) {
+      return $cache->data;
+    }
+    try {
+      // Get the entity query for the reqres_user entity.
+      $query = $this->entityTypeManager->getStorage('reqres_user')->getQuery();
+      // Add tag to the query for altering.
+      $query->addTag('reqres_users_query');
+      $query->accessCheck(TRUE);
+
+      // Count the total number of rows.
+      $total_rows = $query->count()->execute();
+      // Cache successful response.
+      $this->cache->set($cid, $total_rows, CacheBackendInterface::CACHE_PERMANENT, ['reqres_users_data']);
+
+      return $total_rows;
+    }
+    catch (Exception $e) {
+      \Drupal::logger('reqres_users')->error('Failed fetching users from Reqres API: @message', ['@message' => $e->getMessage()]);
+
+      return [];
+    }
   }
 
 }
